3fa2df2859b694eef9589b5cf5228561e7f322fb
Sukhman Singh <pgv1165@autuni.ac.nz>
2021-04-28 21:22:41 +1200

    App functional

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
     appBar: {
         margin: '30px 0',
         display: 'flex',
@@ -14,4 +14,9 @@ export default makeStyles(() => ({
       image: {
         marginRight: '10px',
       },
+      [theme.breakpoints.down('sm')]: {
+        mainContainer: {
+          flexDirection:"column-reverse"
+        }
+      }
 }));
