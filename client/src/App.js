0d54832b895fea5edf9393a300a96f1caf0f60d0
Sukhman Singh <pgv1165@autuni.ac.nz>
2021-05-25 21:58:32 +1200

    Table sorting and filtering per column working

--- client/src/App.js
+++ client/src/App.js
@@ -24,31 +24,7 @@ const App = () => {
                 <img className={classes.image} src={Logo} alt="SEEDS" height="70" />
                 <Typography className = {classes.heading} variant="h4" align="center">SEEDS</Typography>
             </AppBar>
-            <Grow in>
-            <Container>
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Grid item xs={12} sm={12}>
-                <Typography className = {classes.heading} variant="h4" align="center">Search Filters</Typography>
-                <Typography className = {classes.heading} variant="h4" align="center"> 
-                <select>
-                    <option selected value="sepractice">Select SE Practice</option>
-                    <option value="tdd">TDD</option>
-                </select>
-                <select>
-                    <option selected value="claim">Select Claim</option>
-                    <option value="c1">Improves Teamwork</option>
-                    <option value="c2">Improves Code Quality</option>
-                    <option value="c3">Improves Productivity</option>
-                    <option value="c4">Improves Code Output</option>
-                </select>
-                <button onclick="">Search</button>
-                </Typography>
-                </Grid>
-                </AppBar>
-            </Container>
-            </Grow>
-                <Container>
-                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={6}>
                     <Grow in>
                         <Grid item xs={12} sm={12}>
                             <Posts setCurrentId={setCurrentId} />
@@ -60,7 +36,6 @@ const App = () => {
                         </Grid>
                         </Grow>
                     </Grid>
-                </Container>
         </Container>
     )
 }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,44 +2,34 @@ import React from 'react';
 import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
+import { DataGrid } from '@material-ui/data-grid';
 import Post from './Post/Post';
 import useStyles from './styles';
+
+const columns = [
+    { field: 'title', headerName: 'Title', width: 400},
+    { field: 'author', headerName: 'Author(s)', width: 200 },
+    { field: 'year', headerName: 'Year', width: 100},
+    { field: 'sepractice', headerName: 'SE Practice', width: 127},
+    { field: 'claim', headerName: 'Claim', width: 180},
+    { field: 'evidencestrength', headerName: 'Strength of Evidence', width: 190},
+];
 
 const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+    var id = 0;
+    const rows = 
+    posts.map((post) => (
+        { id: id++, title: post.title, author: post.author, year: post.year, 
+        sepractice: post.sepractice, claim: post.claim, evidencestrength: post.evidencestrength}
+    ));
 
     return (
         !posts.length ? <CircularProgress /> : (
-            <Grid className = {classes.container} container>
-                <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Title</TableCell>
-                            <TableCell>Author(s)</TableCell>
-                            <TableCell>Year</TableCell>
-                            <TableCell>SE Practice</TableCell>
-                            <TableCell>Claim</TableCell>
-                            <TableCell>Evidence Strength</TableCell>
-                        </TableRow>
-                    </TableHead>
-                {posts.map((post) => (
-                    <TableBody key={post._id} item xs={12} sm={12}>
-                            <Post post={post} setCurrentId={setCurrentId} />
-                    </TableBody>
-                ))}
-                </Table>
-                </TableContainer>
-            </Grid>
+            <div style={{ height: 650, width: '100%', backgroundColor: 'white'}}>
+            <DataGrid rows={rows} columns={columns} pageSize={10}/> 
+            </div>
         )
     );
 };
